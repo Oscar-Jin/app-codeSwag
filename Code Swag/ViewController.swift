31fa0a4d5044074afdf50332b59ffd8c9148ef72
Oscar-Jin <kintininn@gmail.com>
2019-05-27 13:24:12 +0900

    Initial Commit

---
+++ Code Swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Swag
+//
+//  Created by Zhiren Jin on 2019/05/27.
+//  Copyright © 2019 Zhiren Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
