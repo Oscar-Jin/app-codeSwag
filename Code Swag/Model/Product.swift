53958bd5ff35803a53081f5dc7ea8b2a44a36c70
Oscar-Jin <kintininn@gmail.com>
2019-05-28 13:01:48 +0900

    collection view added

---
+++ Code Swag/Model/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  Code Swag
+//
+//  Created by Zhiren Jin on 2019/05/28.
+//  Copyright © 2019 Zhiren Jin. All rights reserved.
+//
+
+import Foundation
+struct Product {
+    var title: String
+    var price: String
+    var imageName: String
+}
---
+++ Code Swag/View/ProductCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCollectionViewCell.swift
+//  Code Swag
+//
+//  Created by Zhiren Jin on 2019/05/28.
+//  Copyright © 2019 Zhiren Jin. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
+}
